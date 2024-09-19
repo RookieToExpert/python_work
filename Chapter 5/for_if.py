c724ad765c3431cb8c11448ec741a28715825e9d
Rui Tang <ruit@vt.edu>
2024-09-18 22:28:32 -0400

    Adding new file on 10:28 9/18/2024

---
+++ Chapter 5/for_if.py
@@ -0,0 +1,9 @@
+toppings = ['mushrooms', 'peppers', 'cheese']
+
+for topping in toppings:
+    if topping == 'peppers':
+        print(f"Sorry, we run out of {toppings[1]} now!")
+    else:
+        print(f"Adding {topping}!!")
+
+        
