bf38d6e5bc155fa8715005b7f887278a60df116a
ray <rui0821722@gmail.com>
2024-09-19 06:56:11 +0000

    9/19/2024 ray

--- Chapter 6/4.Looping_dictionary.py
+++ Chapter 6/4.Looping_dictionary.py
@@ -24,3 +24,28 @@ favorite_languages = {
 
 for name, language in favorite_languages.items():
     print(f"\n{name.title()}'s favorite coding language is {language.title()}")
+
+
+#Looping only key in a dictionary
+#Looping through the keys is actually the default behavior when looping 
+#through a dictionary
+#But putting a keys() method can make your code more readable
+for name in favorite_languages.keys():
+    print(f"Introducing {name.upper()} to you!")
+
+#If and for loop work together with dicitionary
+name_list = ['ray', 'michael', 'jenny', 'ben']
+for name in favorite_languages.keys():
+    print(f"Hi, {name.title()}, good to see you!")
+
+    if name in name_list:
+        print(f"Hi {name.title()}, I know your favorite language is {favorite_languages[name]} ")
+
+
+#If and for loop work together with dicitionary
+name_list = ['ray', 'michael', 'jenny', 'ben']
+for name in name_list:
+    print(f"Hi, {name.title()}, good to see you!")
+
+    if name in favorite_languages.keys():
+        print(f"Hi {name.title()}, I know your favorite language is {favorite_languages[name]} ")
