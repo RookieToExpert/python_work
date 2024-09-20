32e81a1cc46836cc0628bc9d164ecbddcb3b0ba0
ray <rui0821722@gmail.com>
2024-09-20 09:50:00 +0000

    9/20/2024 Ray

--- Chapter 6/7.6.7_work.py
+++ Chapter 6/7.6.7_work.py
@@ -20,7 +20,8 @@ person_2 = {
     'age' : 25,
     'city' : 'shanghai'
 }
-
+#Nest the dictionary into a list
+#Print everything in the list that contains dictionary
 people = [person_0, person_1, person_2]
 for person in people:
     for key, value in person.items():
@@ -28,4 +29,27 @@ for person in people:
             print(f"The city where the pEople lives is: {value}")
         else:
             print(f"The {key} of this person is: {value}")
-print("......")
+print("-"*20, "\n")
+
+#6.8 Pets
+pet_0 = {
+    'name' : 'fantuan',
+    'owner' : 'michael'
+}
+
+pet_1 = {
+    'name' : 'anbei',
+    'owner' : 'niang'
+}
+
+pet_2 = {
+    'name' : 'daidai',
+    'owner' : 'ray'
+}
+
+pet_list = [pet_0, pet_1, pet_2]
+
+for pet in pet_list:
+    for name in pet.values():
+        print(f"The pet's name is {name}, and its owner is {name.title()}")
+print("-"*20, "\n")
